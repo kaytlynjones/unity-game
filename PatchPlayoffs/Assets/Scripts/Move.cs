9d7531ee82cec50cf6360710b04dced2ddf4e8d8
kaytlynjones <kjones405@gatech.edu>
2023-11-24 21:52:14 -0500

    added rymar movement
    use left and right keys to rotate, and up and down keys to move forward;

---
+++ PatchPlayoffs/Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private float moveSpeed, rotateSpeed, move, rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;
+        rotateSpeed = 100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        rotation = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        transform.Translate(Vector3.right * move);
+        transform.Rotate(0f, rotation, 0f);
+    }
+}
