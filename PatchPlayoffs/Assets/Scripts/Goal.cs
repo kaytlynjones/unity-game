2b173576fd615fcdfb49d04c4f5812658c126e23
Hannah Wysocki <hwysocki22@gmail.com>
2023-11-28 20:54:05 -0500

    3 goals take you to win screen

--- PatchPlayoffs/Assets/Scripts/Goal.cs
+++ PatchPlayoffs/Assets/Scripts/Goal.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 public class Goal : MonoBehaviour {
     public TMP_Text scoreText;
     public Rigidbody rb;
     int score;
-    public TextMeshProUGUI scoreText;
     // Start is called before the first frame update
     void Awake() {
         score = 0;
@@ -15,6 +15,9 @@ public class Goal : MonoBehaviour {
         if (other.gameObject.CompareTag("Ball")){
             //goal scored
             score++;
+            if (score == 3) {
+                SceneManager.LoadScene("WinScreen");
+            }
             scoreText.text = "Score: " + score;
             rb.position = new Vector3(-10f, 1.478365f, -5f);
             rb.velocity = new Vector3(0,0,0);
