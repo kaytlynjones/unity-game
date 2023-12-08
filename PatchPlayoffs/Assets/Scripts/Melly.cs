14feb94a1076cb206b6e5dcd786acfadbf38ce54
Hannah Wysocki <hwysocki22@gmail.com>
2023-12-08 15:03:49 -0500

    Prevented goalie from being knocked over

--- PatchPlayoffs/Assets/Scripts/Melly.cs
+++ PatchPlayoffs/Assets/Scripts/Melly.cs
@@ -18,20 +18,9 @@ public class Melly : MonoBehaviour
     void Update()
     {
         Vector3 position = transform.position;
-        // transform.Translate(dir * speed * Time.deltaTime);
-        
-        // if (transform.position.x >= -85) {
-        //     dir = Vector3.right;
-        // } else if (transform.position.x <= -69) {
-        //     dir = Vector3.left;
-        // }
         float newX = Mathf.Sin(Time.time * speed);
-        Debug.Log(newX);
+ 
         Vector3 move = new Vector3(maxMove * newX, 0f, 0f);
-        
         transform.Translate(move);
-        // transform.Translate(Vector3.right * newX);
-
-        // transform.Translate(Vector3.right * newX);
     }
 }
